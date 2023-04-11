1b23d51a00a00608440c08588c203c78d840c238
priyanshu gupta <103685294+Priyanshu0520@users.noreply.github.com>
2023-04-11 09:54:18 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:news/pages/home.dart';
+
+void main ()
+{
+  runApp(MaterialApp(
+    
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+    
+  ));
+}
